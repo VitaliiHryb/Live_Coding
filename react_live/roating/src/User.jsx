24a9208301d0aa78226b7a6c5815266e7aa5d5da
VitaliiHryb <vitaliigryb@ukr.net>
2022-10-17 14:54:44 +0300

    React Router - Вложенные роуты, Users

--- react_live/roating/src/User.jsx
+++ react_live/roating/src/User.jsx
@@ -1,18 +1,59 @@
 import React from 'react';
+import { useState, useEffect, useParams } from 'react';
 
 const User = props => {
   // console.log(props.location.pathname);
+  const userId = props.match.params.userId;
+  // const { userId } = useParams(); // is is the same as const userId = props.match.params.userId;
+
+  const [userData, setUserData] = useState(null);
+
+  // useState
+  // input: any data type
+  // out:  array of two entries
+
+  // state
+  // userName
+  // userAvatar
+  // userLocation
+
+  // http request
+
+  // useEffect
+  // input: callback, array;
+  // output: undefined;
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${userId}`)
+      .then(response => response.json())
+      .then(res => {
+        // console.log('res', res);
+        const { name, avatar_url, location } = res;
+        setUserData({
+          userName: name,
+          userAvatar: avatar_url,
+          userLocation: location,
+        });
+      });
+  }, [userId]);
+
+  if (!userData) {
+    return null;
+  }
+
+  const { userName, userAvatar, userLocation } = userData;
 
   return (
     <div className="user">
       <img
         alt="User Avatar"
-        src="https://avatars.githubusercontent.com/u/9919?v="
+        // src="https://avatars.githubusercontent.com/u/9919?v="
+        src={userAvatar}
         className="user__avatar"
       />
       <div className="user__info">
-        <span className="user__name">Github</span>
-        <span className="user__location">USA</span>
+        <span className="user__name">{userName}</span>
+        <span className="user__location">{userLocation}</span>
       </div>
     </div>
   );
